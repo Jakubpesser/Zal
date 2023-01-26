1950dbcf6a8e5f4abe9895ff02f7db5c2b1c1c18
Jakubpesser <115145147+Jakubpesser@users.noreply.github.com>
2023-01-26 11:41:04 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,27 @@
-radio.setFrequencyBand(7)
-radio.setGroup(1)
-radio.setTransmitPower(7)
-radio.setTransmitSerialNumber(true)
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5) 
+})
 
-
-
+input.onLogoEvent(TouchButtonEvent.Pressed, function() {
+    control.reset();
+})
+radio.onReceivedNumber(function(receivedNumber: number){
+    console.log(receivedNumber)
+})
 radio.onReceivedValue(function(name: string, value: number){
     console.log(name)
     console.log(value)
+})
+let serialRemote = 7
+radio.setFrequencyBand(7)
+radio.setTransmitPower(7)
+radio.setGroup(22) 
+radio.setTransmitSerialNumber(true)
+console.logValue("name", Utility.encodeSerial())
+
+radio.onReceivedNumber(function(receivedNumber: number) {
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
+})
+radio.onReceivedNumber(receivedNumber: number){
+    basic.showString(number)
 })
